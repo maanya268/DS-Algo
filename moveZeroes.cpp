f96577fc681c163a876ed3db19a5f36554716dce
maanya268 <74601983+maanya268@users.noreply.github.com>
2022-02-02 23:54:00 +0530

    Create moveZeroes.cpp

---
+++ moveZeroes.cpp
@@ -0,0 +1,20 @@
+move zeroes to end
+
+    void moveZeroes(vector<int>& nums) 
+    {
+        int j = 0;
+        int n = nums.size();
+        
+        for(int i = 0;i<n;i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[j++] = nums[i];
+            }
+        }
+        
+        while(j<n)
+        {
+            nums[j++] = 0;
+        }
+    }
