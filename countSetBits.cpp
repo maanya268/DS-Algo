5a236a5eb46921c1c423b47b733cb34f7df7a00a
maanya268 <74601983+maanya268@users.noreply.github.com>
2022-02-09 16:49:07 +0530

    Add files via upload

---
+++ countSetBits.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int count = 0;
+    while (n)
+    {
+        count += 1;
+        n = (n >> 1);
+    }
+    cout << count << endl;
+    return 0;
+}
